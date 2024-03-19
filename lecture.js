c49e50c9931087fab1d1647e884a4f54b5451cd8
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-03-19 12:56:42 +0500

    SMIT Expo- ReactNative

---
+++ lecture.js
@@ -0,0 +1,88 @@
+/*
+Class 1
+// React Native
+Cross Platform Mobile Application Framework
+
+For Android Development---> Java/Kotlin 
+For ISO Development --> Swift/Objective C
+
+Cross Platform
+1. Ionic(Angular JS 80%Fail)(ye apni application web pe chala hai)
+2. React Native(React, JavaScipt)
+3. Flutter(Dart)
+
+JavaScript ==> Web(React), Mobile Apps(React-Native), Desktop(ElectronJS)
+Dart ==> Mobile Apps, Web(little support)
+
+
+Performance
+Winnder: Flutter
+
+Community:
+Winner: React-Native
+
+====================
+React-Native
+1. Expo
+2. ReactNative CLI
+
+1. Expo
+Pros:
+    * You don't need Android Studio or Xcode for starting , building and Developing the App 
+    * You can see the output of both Android/IOS during development
+    * You don't need much "Configuration" to launch the app.
+    * npx create-expo-app myApp
+COns:
+    * Developent is little Slow as compare to ReactNative CLI
+    * App Size is usually large for e.g 25-30MB
+
+2. RN CLI
+Pros:
+    * Developent is Fast as compare to Expo
+    * App Size is usually small, For e.g 5MB
+
+Cons:
+    * You need Android Studio or Xcode for builing and developing the App.
+    * You can only see the output of IOS in Mac-OS . for Android any OS is enough 
+    * You will need multiple configuration and libraries to launch the app
+    * 
+
+=========
+Charges
+Playstore: 25$/Zindagi
+AppStore: 100$/year
+
+
+MacOS Alternatives:
+// 1. Client sy bheek manglo! Mac OS Gift karde!
+1. Client ko bolo Mac OS Gift karde!
+2. VMWare(1 hour)(Virtual-Machine)
+3. Hackintosh( Windows Operating System change to MAC-OS)
+4.
+
+*/
+
+
+/* 
+========Class 2
+
+Stylesheet.create <--- agar ap use nhi kro too wo recomandation nhi dyga styling me
+Stylesheet.create <--- laken phir css me galti hogi too wo error nhi dyga, or intelisence bbhi nhi dyga
+
+
+    "UI" k liye "React-Native" ki Documentation use kren
+    'Hardware' wagera k liye "Expo" ki Documentation use kren
+
+
+*/
+
+/* 
+======Class 3
+
+follow folder structure
+src/components
+camera install from expo
+
+
+
+*/
