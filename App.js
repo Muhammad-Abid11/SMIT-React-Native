666816a16aa8fd5ca3b7e27096639818a7442db4
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-03-21 09:20:44 +0500

    NativeBase RN library

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Class1 from './src/views/Class1'
+import Class1Library from './src/views/Class1_library'
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <Class1 />
+      {/* <Class1 /> */}
+      <Class1Library />
     </View>
   );
 }
@@ -15,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
